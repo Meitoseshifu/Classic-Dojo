cb0a22226a900420895537b8caedb599ab8afe4d
Andriy Paliychuk <wantstraddle@gmail.com>
2022-02-16 21:31:08 +0200

    add NameInvert kata

--- src/main/java/ua/happy/learning/name/Inverter.java
+++ src/main/java/ua/happy/learning/name/Inverter.java
@@ -1,5 +1,9 @@
 package ua.happy.learning.name;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Given String with name
  * "John Smith" => "Smith, John"
@@ -8,4 +12,38 @@ package ua.happy.learning.name;
  * " John      Smith" => "Smith, John"
  */
 public class Inverter {
+    private static final String ANY_AMOUNT_OF_WHITESPACE = "\\s+";
+
+    public static String invert(String name) {
+        List<String> nameParts = new ArrayList<>(breakIntoPartsIgnoringWhitespace(name));
+
+        if(isHonorific(nameParts.get(0))) nameParts.remove(0);
+
+        return nameParts.size() < 2 ? invertSingleName(nameParts.get(0)) : invertFullName(nameParts);
+    }
+
+    private static String invertFullName(List<String> nameParts) {
+        return String.format("%s, %s %s", nameParts.get(1), nameParts.get(0), findAndMergePostnominals(nameParts)).trim();
+    }
+
+    private static String invertSingleName(String name) {
+        return String.format("%s", name);
+    }
+
+    private static String findAndMergePostnominals(List<String> nameParts) {
+        StringBuilder postnominal = new StringBuilder();
+        for (String namePart : nameParts.subList(2, nameParts.size())) {
+            postnominal.append(namePart).append(" ");
+        }
+        return postnominal.toString();
+    }
+
+    private static boolean isHonorific(String namePart) {
+        List<String> honorifics = List.of("Mr.", "Mrs.");
+        return honorifics.contains(namePart);
+    }
+
+    private static List<String> breakIntoPartsIgnoringWhitespace(String name) {
+        return Arrays.asList(name.trim().split(ANY_AMOUNT_OF_WHITESPACE));
+    }
 }
--- src/test/java/ua/happy/learning/name/InverterTest.java
+++ src/test/java/ua/happy/learning/name/InverterTest.java
@@ -1,5 +1,12 @@
 package ua.happy.learning.name;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Given String with name
  * "John Smith" => "Smith, John"
@@ -8,4 +15,51 @@ package ua.happy.learning.name;
  * " John      Smith" => "Smith, John"
  */
 public class InverterTest {
+
+    @Test
+    @Order(1)
+    public void invert_null_throw_NPE() {
+        assertThrows(NullPointerException.class, () -> Inverter.invert(null));
+    }
+
+    @Test
+    @Order(2)
+    public void invert_emptyString_return_emptyString() {
+        assertThat(Inverter.invert(""), is(""));
+        assertThat(Inverter.invert("     "), is(""));
+    }
+
+    @Test
+    @Order(3)
+    public void invert_firstName_return_firstName() {
+        assertThat(Inverter.invert("John"), is("John"));
+        assertThat(Inverter.invert("     John   "), is("John"));
+    }
+
+    @Test
+    @Order(4)
+    public void invert_firstLast_return_lastCommaFirst() {
+        assertThat(Inverter.invert("John Smith"), is("Smith, John"));
+        assertThat(Inverter.invert("John      Smith"), is("Smith, John"));
+    }
+
+    @Test
+    @Order(5)
+    public void invert_honorificsFirstLast_return_lastCommaFirst() {
+        assertThat(Inverter.invert("Mr. John Smith"), is("Smith, John"));
+        assertThat(Inverter.invert("Mrs. Jane Smith"), is("Smith, Jane"));
+    }
+
+    @Test
+    @Order(6)
+    public void invert_firstLastPostnominals_return_lastCommaFirstPostnominals() {
+        assertThat(Inverter.invert("John Smith Sr."), is("Smith, John Sr."));
+        assertThat(Inverter.invert("John Smith Sr. PhD."), is("Smith, John Sr. PhD."));
+    }
+
+    @Test
+    @Order(6)
+    public void invert_acceptanceTest() {
+        assertThat(Inverter.invert("Mr.         John Smith     Sr.      PhD."), is("Smith, John Sr. PhD."));
+    }
 }
