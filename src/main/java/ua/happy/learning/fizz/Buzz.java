6126d8f7884097953a9795b6c14d980bff7cbdfb
Andriy Paliychuk <wantstraddle@gmail.com>
2022-02-16 17:47:52 +0200

    add FizzBuzz kata

--- src/main/java/ua/happy/learning/fizz/Buzz.java
+++ src/main/java/ua/happy/learning/fizz/Buzz.java
@@ -9,4 +9,38 @@ package ua.happy.learning.fizz;
  */
 public class Buzz {
 
+    private static final String ZERO = "0";
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "FizzBuzz";
+
+
+    public static String of(int digit) {
+        if(digit == 0) return ZERO;
+        if(isFizz(digit) && isBuzz(digit)) return FIZZBUZZ;
+        if(isFizz(digit)) return FIZZ;
+        if(isBuzz(digit)) return BUZZ;
+        return String.valueOf(digit);
+    }
+
+    /*public static String of(int digit) {
+        if(digit == 0) return "0";
+
+        String result = "";
+
+        if(isFizz(digit))
+            result += "Fizz";
+        if(isBuzz(digit))
+            result += "Buzz";
+
+        return result.isEmpty() ? String.valueOf(digit) : result;
+    }*/
+
+    private static boolean isFizz(int i) {
+        return i % 3 == 0;
+    }
+
+    private static boolean isBuzz(int i) {
+        return i % 5 == 0;
+    }
 }
--- src/test/java/ua/happy/learning/fizz/BuzzTest.java
+++ src/test/java/ua/happy/learning/fizz/BuzzTest.java
@@ -1,5 +1,10 @@
 package ua.happy.learning.fizz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * Given an integer greater than or equal to zero
  * if divisible by 3, return "Fizz"
@@ -8,5 +13,40 @@ package ua.happy.learning.fizz;
  * if none og the above, return index
  */
 public class BuzzTest {
+
+    @Test
+    public void of_0_is_0() {
+        assertThat(Buzz.of(0), is("0"));
+    }
+
+    @Test
+    public void of_1_is_1() {
+        assertThat(Buzz.of(1), is("1"));
+    }
+
+    @Test
+    public void of_3_is_Fizz() {
+        assertThat(Buzz.of(3), is("Fizz"));
+    }
+
+    @Test
+    public void of_6_is_Fizz() {
+        assertThat(Buzz.of(6), is("Fizz"));
+    }
+
+    @Test
+    public void of_5_is_Buzz() {
+        assertThat(Buzz.of(5), is("Buzz"));
+    }
+
+    @Test
+    public void of_10_is_Buzz() {
+        assertThat(Buzz.of(10), is("Buzz"));
+    }
+
+    @Test
+    public void of_15_is_FizzBuzz() {
+        assertThat(Buzz.of(15), is("FizzBuzz"));
+    }
 
 }
